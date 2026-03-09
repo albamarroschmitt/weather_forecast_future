78fcf395c11a09f871facaa5fe906b41670f1b3f
albamarroschmitt <albamarroschmitt@gmail.com>
2026-03-09 11:26:11 +0100

    Basic app settings

---
+++ src/index.js
@@ -0,0 +1,73 @@
+function updateWeather(response) {
+  let temperature = Math.round(response.data.temperature.current);
+
+  let temperatureElement = document.querySelector(
+    "#weather-current-temperature",
+  );
+  let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-icon");
+
+  temperatureElement.innerHTML = temperature;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  let description = response.data.condition.description;
+  iconElement.src = response.data.condition.icon_url;
+  iconElement.alt = response.data.condition.description;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function searchCity(cityName) {
+  let apiKey = "9a9b8262d5b0t51o000cfc108a873741";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+    cityName,
+  )}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function searchSubmit(event) {
+  event.preventDefault();
+
+  let searchInputCity = document.querySelector("#city");
+  searchCity(searchInputCity.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
+searchCity("Barcelona");
